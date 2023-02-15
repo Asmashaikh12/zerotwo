85a6cd9251dc07ba9e594a2876265949fce6a849
Asma Shaikh <119008360+Asmashaikh12@users.noreply.github.com>
2023-02-15 13:23:03 +0530

    Add files via upload

--- ZeroTwo/send.py
+++ ZeroTwo/send.py
@@ -23,46 +23,21 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-from ZeroTwo import LOAD, LOGGER, NO_LOAD
+
+from ZeroTwo import dispatcher
+from ZeroTwo.modules.disable import DisableAbleCommandHandler
+from ZeroTwo.modules.helper_funcs.alternate import send_message
+from ZeroTwo.modules.helper_funcs.chat_status import dev_plus
 
 
-def __list_all_modules():
-    import glob
-    from os.path import basename, dirname, isfile
-
-    # This generates a list of modules in this folder for the * in __main__ to work.
-    mod_paths = glob.glob(f"{dirname(__file__)}/*.py")
-    all_modules = [
-        basename(f)[:-3]
-        for f in mod_paths
-        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
-    ]
-
-    if LOAD or NO_LOAD:
-        to_load = LOAD
-        if to_load:
-            if not all(
-                any(mod == module_name for module_name in all_modules)
-                for mod in to_load
-            ):
-                LOGGER.error("Invalid loadorder names. Quitting.")
-                quit(1)
-
-            all_modules = sorted(set(all_modules) - set(to_load))
-            to_load = list(all_modules) + to_load
-
-        else:
-            to_load = all_modules
-
-        if NO_LOAD:
-            LOGGER.info(f"Not loading: {NO_LOAD}")
-            return [item for item in to_load if item not in NO_LOAD]
-
-        return to_load
-
-    return all_modules
+@dev_plus
+def send(update, context):
+    args = update.effective_message.text.split(None, 1)
+    creply = args[1]
+    send_message(update.effective_message, creply)
 
 
-ALL_MODULES = __list_all_modules()
-LOGGER.info("Modules to load: %s", str(ALL_MODULES))
-__all__ = ALL_MODULES + ["ALL_MODULES"]
+ADD_CCHAT_HANDLER = DisableAbleCommandHandler("snd", send, run_async=True)
+dispatcher.add_handler(ADD_CCHAT_HANDLER)
+__command_list__ = ["snd"]
+__handlers__ = [ADD_CCHAT_HANDLER]
--- ZeroTwo/sudoers.py
+++ ZeroTwo/sudoers.py
@@ -23,46 +23,37 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
-from ZeroTwo import LOAD, LOGGER, NO_LOAD
+import os
+import time
+
+import psutil
+
+import ZeroTwo.modules.sql.users_sql as sql
+import ZeroTwo.utils.formatter as formatter
+from ZeroTwo import StartTime
+
+# Stats Module
 
 
-def __list_all_modules():
-    import glob
-    from os.path import basename, dirname, isfile
+async def bot_sys_stats():
+    bot_uptime = int(time.time() - StartTime)
+    cpu = psutil.cpu_percent()
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    process = psutil.Process(os.getpid())
+    users = sql.num_users()
+    chats = sql.num_chats()
+    stats = f"""
+➤ Zero Two's Current System Stats
 
-    # This generates a list of modules in this folder for the * in __main__ to work.
-    mod_paths = glob.glob(f"{dirname(__file__)}/*.py")
-    all_modules = [
-        basename(f)[:-3]
-        for f in mod_paths
-        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
-    ]
-
-    if LOAD or NO_LOAD:
-        to_load = LOAD
-        if to_load:
-            if not all(
-                any(mod == module_name for module_name in all_modules)
-                for mod in to_load
-            ):
-                LOGGER.error("Invalid loadorder names. Quitting.")
-                quit(1)
-
-            all_modules = sorted(set(all_modules) - set(to_load))
-            to_load = list(all_modules) + to_load
-
-        else:
-            to_load = all_modules
-
-        if NO_LOAD:
-            LOGGER.info(f"Not loading: {NO_LOAD}")
-            return [item for item in to_load if item not in NO_LOAD]
-
-        return to_load
-
-    return all_modules
-
-
-ALL_MODULES = __list_all_modules()
-LOGGER.info("Modules to load: %s", str(ALL_MODULES))
-__all__ = ALL_MODULES + ["ALL_MODULES"]
+────────────────────────
+• UPTIME: {formatter.get_readable_time((bot_uptime))}
+• BOT: {round(process.memory_info()[0] / 1024 ** 2)} MB
+• CPU: {cpu}%
+• RAM: {mem}%
+• DISK: {disk}%
+• CHATS: {chats}
+• USERS: {users}
+────────────────────────
+"""
+    return stats
